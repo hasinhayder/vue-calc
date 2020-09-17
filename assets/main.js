724f8e9dddb9ad3500c44b39a20eace5395f6242
Hasin Hayder <countdraculla@gmail.com>
2020-09-17 22:45:19 +0600

    cleanup and refactoring

--- assets/main.js
+++ assets/main.js
@@ -1,49 +1,48 @@
 Vue.createApp({
-  data () {
-    return {
-      del: localStorage.delActive ? localStorage.delActive : false,
-      label: '',
-      item: '',
-      items: []
-    }
-  },
-  watch: {
-    del (value) {
-      localStorage.delActive = value
-    }
-  },
-  computed: {
-    sum () {
-      return this.items.length > 0
-        ? this.items.reduce((o, n) => (o = o + n))
-        : 0
-    }
-  },
-  mounted () {
-    this.updateTotal()
-  },
-  methods: {
-    addItem () {
-      this.item = eval(this.item.replace(/[^-()\d/*+.]/g, '')) //parse the math expression, but sanitize it
-      if (this.item > 0) {
-        this.items.unshift(this.item * 1) //or push - whatever works for you
-      }
-      this.updateTotal()
-    },
-    removeItem (n) {
-      this.items.splice(n, 1)
-      this.updateTotal()
-    },
-    undoAddition () {
-      if (this.del) {
-        this.item == '' ? this.items.shift() : false
-        this.updateTotal()
+    data () {
+      return {
+        del: localStorage.delActive ? localStorage.delActive : false,
+        label: '',
+        item: '',
+        items: []
       }
     },
-    updateTotal () {
-      this.item = ''
-      this.label = this.sum == 0 ? '' : 'Total = ' + this.sum
-      this.$refs.n.focus()
+    watch: {
+      del (value) {
+        localStorage.delActive = value
+      }
+    },
+    computed: {
+      sum () {
+        return this.items.length > 0
+          ? this.items.reduce((o, n) => (o = o + n))
+          : 0
+      }
+    },
+    mounted () {
+      this.focus()
+    },
+    methods: {
+      addItem () {
+        this.item = eval(this.item.replace(/[^-()\d/*+.]/g, '')) //parse the math expression, but sanitize it
+        if (this.item > 0) {
+          this.items.unshift(this.item * 1) //or push - whatever works for you
+        }
+        this.item = '';
+      },
+      removeItem (n) {
+        this.items.splice(n, 1)
+      },
+      undoAddition () {
+        if (this.del) {
+          this.item == '' ? this.items.shift() : false
+          this.focus()
+        }
+      },
+      focus () {
+        this.item = ''
+        this.$refs.n.focus()
+      }
     }
-  }
-}).mount('#app')
+  }).mount('#app')
+  
